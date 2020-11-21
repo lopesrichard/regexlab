5612d654502db3c984f60c1c17fa50a5df9b908c
lopesrichard <contato.richardlopes@outlook.com>
2020-11-21 19:20:19 -0300

    feat: save font preferences

--- app/src/assets/js/main.js
+++ app/src/assets/js/main.js
@@ -1,24 +1,37 @@
-const html = document.querySelector('html');
-const body = document.querySelector('body');
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
-const fonts  = {
-    flower: 'Indie Flower', courier: 'Courier', roboto: 'Roboto', slabo: 'Slabo',
-    times: 'Times New Roman', ubuntu: 'Ubuntu',
+const fonts = {
+  flower: "Indie Flower",
+  courier: "Courier",
+  roboto: "Roboto",
+  slabo: "Slabo",
+  times: "Times New Roman",
+  ubuntu: "Ubuntu",
 };
-const select = document.getElementById('fonts');
+
+const select = document.getElementById("fonts");
 select.innerHTML = setOptions();
 
 function setOptions() {
-    let options = '';
-    for (let font in fonts) {
-        options += `<option value="${font}">${fonts[font]}</option>`;
-    }
-    return options;
+  let options = "";
+  for (let font in fonts) {
+    options += `<option value="${font}">${fonts[font]}</option>`;
+  }
+  return options;
 }
 
-select.addEventListener('input', function() {
-    for (let font in fonts) {
-        body.classList.remove(font);
-    }
-    body.classList.add(select.value);
+const font = localStorage.getItem("font");
+
+if (font) {
+  body.classList.add(font);
+  select.value = font;
+}
+
+select.addEventListener("input", function () {
+  for (let font in fonts) {
+    body.classList.remove(font);
+  }
+  body.classList.add(select.value);
+  localStorage.setItem("font", select.value);
 });
